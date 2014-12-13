bc2e19f8a99491767242ee3fab557756fd9b5d45
Joe Sortelli <joe@sortelli.com>
2014-12-12 18:09:53 -0800

    Add test_already_solved unit test

--- test/basic.rb
+++ test/basic.rb
@@ -1,6 +1,12 @@
 require "minitest/autorun"
 require "bfs_brute_force"
 
+class AlreadySolvedContext < BfsBruteForce::Context
+  def solved?
+    true
+  end
+end
+
 class TestBasic < Minitest::Unit::TestCase
   def test_module_exists
     mod_key = :BfsBruteForce
@@ -10,5 +16,16 @@ class TestBasic < Minitest::Unit::TestCase
     %w{State Context Solver}.each do |c|
       assert mod.const_defined?(c), "Class #{mod}::#{c} missing"
     end
+  end
+
+  def test_already_solved
+
+    context = AlreadySolvedContext.new
+    solver  = BfsBruteForce::Solver.new
+
+    assert_raises(NotImplementedError) {context.next_moves(nil)}
+    assert context.solved?
+
+    solver.solve context, []
   end
 end
