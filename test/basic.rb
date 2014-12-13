e2d7a83a5087a4a565ce907c8610f861f71bf4b5
Joe Sortelli <joe@sortelli.com>
2014-12-12 18:15:42 -0800

    Add test_broken unit test

--- test/basic.rb
+++ test/basic.rb
@@ -7,6 +7,12 @@ class AlreadySolvedContext < BfsBruteForce::Context
   end
 end
 
+class BrokenContext < BfsBruteForce::Context
+  def solved?
+    false
+  end
+end
+
 class TestBasic < Minitest::Unit::TestCase
   def test_module_exists
     mod_key = :BfsBruteForce
@@ -19,7 +25,6 @@ class TestBasic < Minitest::Unit::TestCase
   end
 
   def test_already_solved
-
     context = AlreadySolvedContext.new
     solver  = BfsBruteForce::Solver.new
 
@@ -27,5 +32,15 @@ class TestBasic < Minitest::Unit::TestCase
     assert context.solved?
 
     solver.solve context, []
+  end
+
+  def test_broken
+    context = BrokenContext.new
+    solver  = BfsBruteForce::Solver.new
+
+    assert_raises(NotImplementedError) {context.next_moves(nil)}
+    refute context.solved?
+
+    assert_raises(NotImplementedError) { solver.solve(context, []) }
   end
 end
