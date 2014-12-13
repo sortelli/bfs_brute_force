66f5dc0d1ed78f4c20d9c94ff827a209cf1681a9
Joe Sortelli <joe@sortelli.com>
2014-12-12 17:19:21 -0800

    Initial implementation of Solver

--- lib/bfs_brute_force.rb
+++ lib/bfs_brute_force.rb
@@ -1,5 +1,64 @@
 require "bfs_brute_force/version"
+require "set"
 
 module BfsBruteForce
-  # Your code goes here...
+  class State
+    attr_reader :context, :moves
+
+    def initialize(context, moves = [], already_seen = Set.new)
+      @context      = context
+      @moves        = moves
+      @already_seen = already_seen
+    end
+
+    def solved?
+      @context.solved?
+    end
+
+    def next_states
+      list = []
+      @context.next_moves(@already_seen) do |next_move, next_context|
+        list << State.new(next_context, @moves + [next_move], @already_seen)
+      end
+      list
+    end
+  end
+
+  class Context
+    def solved?
+      raise NotImplementedError, "solved? is not implemented yet"
+    end
+
+    def next_moves(already_seen)
+      raise NotImplementedError, "next_moves is not implemented yet"
+    end
+  end
+
+  class Solver
+    def solve(initial_context)
+      puts "Looking for solution for:\n#{initial_context}\n\n"
+
+      states = [[State.new(initial_context)]]
+
+      loop do
+        puts("Checking for solutions that take %4d moves. %7d new contexts" % [
+          states.length - 1,
+          states.last.length
+        ])
+
+        states.last.each do |state|
+          if state.solved?
+            puts "\nSolved:\n"
+            puts state.moves.map {|m| "  #{m}"}
+            puts "\n#{state.context}"
+            exit 0
+          end
+        end
+
+        states.push(states.last.flat_map {|s| s.next_states})
+
+        raise "There are no more states to analyze" if states.last.size == 0
+      end
+    end
+  end
 end
--- test/basic.rb
+++ test/basic.rb
@@ -1,7 +1,14 @@
 require "minitest/autorun"
+require "bfs_brute_force"
 
 class TestBasic < Minitest::Unit::TestCase
   def test_module_exists
-    assert BfsBruteForce
+    mod_key = :BfsBruteForce
+    assert Kernel.const_defined?(mod_key), "Module #{mod_key} missing"
+
+    mod = Kernel.const_get mod_key
+    %w{State Context Solver}.each do |c|
+      assert mod.const_defined?(c), "Class #{mod}::#{c} missing"
+    end
   end
 end
